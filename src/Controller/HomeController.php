095eec59de37e55efbe80cc90f2e8512351fefa2
titouwork <titouan.pro.lahely@gmail.com>
2023-07-24 09:21:43 +0200

    création deu formulaire contact(entité+template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use DateTimeImmutable;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
 use App\Repository\ContentRepository;
 use App\Repository\ProjectsRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,11 +16,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ContentRepository $contentRepo, ProjectsRepository $projectsRepo): Response
+    public function index(
+    ContentRepository $contentRepo, 
+    ProjectsRepository $projectsRepo,
+    ContactRepository $contactRepository,
+    Request $request
+    ): Response
     {
+        $contact= new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact->setDate(new DateTimeImmutable('now'));
+            $contactRepository->save($contact, true);
+            return $this->redirectToRoute('app_validation');
+        }
         return $this->render('home/index.html.twig', [
             'content' => $contentRepo->findAll(),
             'projects' => $projectsRepo->findAll(),
+            'contactForm' => $form->createView(),
         ]);
     }
 }
---
+++ src/Entity/Contact.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContactRepository::class)]
+class Contact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $object = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->name = $Name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getObject(): ?string
+    {
+        return $this->object;
+    }
+
+    public function setObject(string $object): self
+    {
+        $this->object = $object;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
